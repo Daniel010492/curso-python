8bf0a978247720481f229328120dea37040d45f1
Daniel010492 <d.a.cruz.rojas@gmail.com>
2020-12-28 03:16:21 +0000

    mi codigo

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+uthor: Daniel Antonio Cruz Rojas
+# Date: Oct 3, 2020
+# Universidad Veracruzana
+# helloworld.py
+
+print ("Hello world python")
+print ("Hello universe ...")
+print ("Welcome to python !!")
+
+
